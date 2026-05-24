b22cc4afc8f33df77ef4a83072232bff17b786ff
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2026-05-25 03:05:30 +0530

    Change OWNER_ID to accept multiple values

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     API_HASH = getenv("API_HASH")
     BOT_TOKEN = getenv("BOT_TOKEN")
     
-    OWNER_ID = int(getenv("OWNER_ID", "7044783841"))
+    OWNER_ID = set(map(int, getenv("OWNER_ID", "7625845619").split()))
 
     START_PIC = getenv("START_PIC", "https://files.catbox.moe/cd2zru.jpg")
